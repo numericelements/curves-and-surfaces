f322bf3626923ad9a936e34384f30652f523f111
jean-claude LEON <jeanclaude.leon2@gmail.com>
2021-06-28 14:50:39 +0200

    small update on refactoring

---
+++ src/bsplineOptimizationProblems/OptimizationProblemCtrlParameters.ts
@@ -0,0 +1,39 @@
+/**
+ * Set of optimizer parameters used to monitor the behavior and type of optimizer used during the shape navigation process
+ * @_activeOptimizer : true if an optimizer is effectivzly active during the navigation process
+ * @_updateConstraintsBounds : true if the bounds set on inequalities constraints of the optimizer can be modified 
+ * to force an optimized to stick to the boundary of a shape space
+ * @_useWeights : true if the optimizer use weights to reduce the rigid body displacements of the curve
+*/
+
+export class OptimizationProblemCtrlParameters {
+
+    private _activeOptimizer: boolean;
+    private _updateConstraintsBounds: boolean;
+    private _useWeights:boolean;
+
+    constructor(activeOptimizer?: boolean, updateConstraintBounds?:boolean) {
+        this._activeOptimizer = false;
+        this._updateConstraintsBounds = false;
+        this._useWeights = false;
+        if(activeOptimizer !== undefined) {
+            this._activeOptimizer = activeOptimizer;
+        }
+        if(updateConstraintBounds !== undefined) {
+            this._updateConstraintsBounds = updateConstraintBounds;
+        }
+    }
+
+    get optimizerStatus(): boolean {
+        return this._activeOptimizer;
+    }
+
+    get updateConstraintBounds(): boolean {
+        return this._updateConstraintsBounds;
+    }
+
+    set optimizerStatus(activeOptimizer: boolean) {
+        this._activeOptimizer = activeOptimizer;
+    }
+
+}
--- src/curveShapeSpaceNavigation/CurveShapeSpaceNavigation.ts
+++ src/curveShapeSpaceNavigation/CurveShapeSpaceNavigation.ts
@@ -1,3 +1,4 @@
+import { OptimizationProblemCtrlParameters } from "../bsplineOptimizationProblems/OptimizationProblemCtrlParameters";
 import { OptimizationProblem_BSpline_R1_to_R2_with_weigthingFactors_general_navigation } from "../bsplineOptimizationProblems/OptimizationProblem_BSpline_R1_to_R2";
 import { BSpline_R1_to_R2 } from "../bsplines/BSpline_R1_to_R2";
 import { CurveDifferentialEventsExtractor } from "../curveShapeSpaceAnalysis/curveDifferentialEventsExtraction";
@@ -16,24 +17,26 @@ export class CurveShapeSpaceNavigator {
     private _selectedControlPoint?: number;
     private currentCurve: BSpline_R1_to_R2;
     private currentControlPolygon: Vector_2d[];
+    private displacementSelctdCP: Vector_2d;
     private seqDiffEventsCurrentCurve: SequenceOfDifferentialEvents;
     private _navigationParameters: ShapeNavigationParameters;
     private _curveConstraints: CurveConstraints;
     private optimizationProblem: OptimizationProblem_BSpline_R1_to_R2_with_weigthingFactors_general_navigation;
     private optimizer: Optimizer;
-    private activeOptimizer: boolean;
+    private _optimizationProblemParam: OptimizationProblemCtrlParameters;
 
     constructor(curveModel: CurveModel) {
         this.curveModel = curveModel;
         this.currentCurve = this.curveModel.spline.clone();
         this.currentControlPolygon = this.currentCurve.controlPoints;
         this._selectedControlPoint = undefined;
+        this.displacementSelctdCP = new Vector_2d(0, 0);
         this._navigationParameters = new ShapeNavigationParameters();
         this._curveConstraints = new CurveConstraints();
         this.seqDiffEventsCurrentCurve = new SequenceOfDifferentialEvents();
         this.optimizationProblem = new OptimizationProblem_BSpline_R1_to_R2_with_weigthingFactors_general_navigation(this.currentCurve.clone(), this.currentCurve.clone());
         this.optimizer = this.newOptimizer(this.optimizationProblem);
-        this.activeOptimizer = false;
+        this._optimizationProblemParam = new OptimizationProblemCtrlParameters();
     }
 
     set navigationParams(navigationParameters: ShapeNavigationParameters) {
@@ -72,11 +75,30 @@ export class CurveShapeSpaceNavigator {
     initializeNavigationStep(): void {
         const diffEventsExtractor = new CurveDifferentialEventsExtractor(this.currentCurve);
         this.seqDiffEventsCurrentCurve = diffEventsExtractor.generateSeqOfDiffEvents();
+        
+    }
+
+    updateCurrentCurve(newCurve: BSpline_R1_to_R2, newSelectedControlPoint: number, newDispSelctdCP: Vector_2d): void {
+        this.currentCurve = newCurve.clone();
+        this.currentControlPolygon = this.currentCurve.controlPoints;
+        this._selectedControlPoint = newSelectedControlPoint;
+        this.displacementSelctdCP = newDispSelctdCP;
+    }
+
+    setTargetCurve(): void {
+        if(this.selectedControlPoint !== undefined) {
+            this.currentCurve.setControlPoint(this.selectedControlPoint, this.displacementSelctdCP);
+        } else {
+            const error = new ErrorLog(this.constructor.name, 'setTargetCurve', 'the index of the selected control point is undefined.');
+            error.logMessageToConsole();
+        }
+        this.optimizationProblem.setTargetSpline(this.currentCurve);
 
     }
 
     updateOptimizerStatus(): void {
-        if(this._navigationParameters.inflectionControl === false && this._navigationParameters.curvatureExtremaControl === false) this.activeOptimizer = false;
+        if(this._navigationParameters.inflectionControl === false && this._navigationParameters.curvatureExtremaControl === false) this._optimizationProblemParam.optimizerStatus = false;
+        if(this._navigationParameters.inflectionControl === true || this._navigationParameters.curvatureExtremaControl === true) this._optimizationProblemParam.optimizerStatus = true;
     }
 
     newOptimizer(optimizationProblem: OptimizationProblem_BSpline_R1_to_R2_with_weigthingFactors_general_navigation) {
